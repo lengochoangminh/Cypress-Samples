664949bec89abc61cc1130f3f8a7165860866809
Minh Le <lengochoangminh@gmail.com>
2023-09-07 14:28:24 +0700

    Part1: QELibrary & UI Utils

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Cypress - Project Samples
 
 ## Branch 0 -  Init the new project with examples from Cypress
+
+## Cypress E2E Template With Typescript 
+ - The template is used for launching a new project 
+ - Part #1: A global variable QE from QELibrary + UI Utils defined the common UI interactions
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,7 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+// import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import { QELibrary } from "./QELibrary";
+global.qe = new QELibrary();
